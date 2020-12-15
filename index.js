c8c28b1c5ec32b645d9c4acffdf96ddd0280c928
jeremylim91 <jeremylim_91@live.com>
2020-12-15 18:07:16 +0800

    added port configs

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import pkg from 'pg';
 
-const PORT = 3004;
+const { Pool } = pkg;
+
+const PORT = process.env.PORT || 3004;
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE,
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: '<MY_UNIX_USERNAME>',
+    host: 'localhost',
+    database: '<MY_UNIX_USERNAME>',
+    port: 5432,
+  };
+}
+const pool = new Pool(pgConnectionConfigs);
 
 // Initialise Express
 const app = express();
@@ -8,7 +29,6 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/bananas', (request, response) => {
-
   const responseText = `This is a random number: ${Math.random()}`;
 
   console.log('request came in', responseText);
@@ -16,6 +36,25 @@ app.get('/bananas', (request, response) => {
   const data = { responseText };
 
   response.render('bananas', data);
+});
+
+app.get('/cats', (request, response) => {
+  console.log('request came in');
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+
+    console.log(result.rows[0].name);
+
+    response.send(result.rows);
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query('SELECT * from cats', whenDoneWithQuery);
 });
 
 app.listen(PORT);
